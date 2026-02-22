1d2c66fca41c362faebbe11c54144a51afdc6bb1
Hannes <257142595+hgb-dev@users.noreply.github.com>
2026-02-22 20:48:57 +0100

    Momentum Pre-Alpha. Loss added argument.

--- optimizers/loss/loss.py
+++ optimizers/loss/loss.py
@@ -1,12 +1,22 @@
-
+import numpy as np
 
 
 class Loss:
     def __init__(self):
         pass
-    def evaluate_loss(self):
+    def evaluate_loss(self, position):
+        """
+        Position is the a numpy array where the loss should be evaluated
+        :param position:
+        :return:
+        """
         pass
-    def evaluate_gradient(self):
+    def evaluate_gradient(self, position):
+        """
+        Position is the a numpy array where the gradient should be evaluated
+
+        :return:
+        """
         pass
 
     
--- optimizers/momentum.py
+++ optimizers/momentum.py
@@ -1,5 +1,35 @@
 import optimizer as opt
+import loss.loss as lo
+import numpy as np
 
 class Momentum(opt.Optimizer):
-        def __init__(self):
+        def __init__(self, lossObject: lo.Loss, decayFactor, learningRate):
             super().__init__()
+
+            self.lossObj = lossObject
+            self.decayFactor = decayFactor
+            self.learningRate = learningRate
+
+            #Previous moment/velocity term
+            self.prevMoment = np.array([])
+            #Current one
+            self.moment = np.array([])
+
+            #Placeholder
+            self.gradLoss = np.array([])
+            #Placeholder for position
+            self.pos = np.array([])
+
+
+        def step(self):
+
+            #Formula for SGD Momentum
+            self.moment = self.prevMoment + self.learningRate * self.lossObj.evaluate_gradient(self.pos)
+            self.pos = self.pos - self.moment
+
+            return self.pos
+
+
+
+
+
