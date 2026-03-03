29c36a1af9ddf3832f59cd8a062a3ef650f62407
Hannes <257142595+hgb-dev@users.noreply.github.com>
2026-03-03 13:40:53 +0100

    fillRandomBatchList first version

--- optimizers/loss/loss.py
+++ optimizers/loss/loss.py
@@ -1,6 +1,7 @@
 import random
 
 import numpy as np
+import math
 
 def get_batches(data, batch_size=50):
     return [data[start:start + batch_size] for start in range(0, len(data), batch_size)]
@@ -9,8 +10,31 @@ class LossObj:
     def __init__(self, data = None, batchAmount = 1):
         self.data = data
         self.batchAmount = batchAmount
+        self.amountOfDataVectors = 1
 
-        self.randomBatchList = []
+        #Get the length of the indata. The other data should be of similar length.
+        self.xDataLength = 1
+        if self.data != None:
+            self.xDataLength = len(self.data[0])
+
+            #Get the amount of data vectors (X,Y,...)
+            self.amountOfDataVectors = len(self.data)
+
+        self.numberOfBatches = math.ceil(self.xDataLength / self.batchAmount)
+
+        self.shuffledData = data
+
+
+        #Allocate memory, the none lists will be replaced by numpy data vectors.
+        #[
+        #    [[None], [None], [None]...],  # batch 0
+        #    [[None], [None], [None]...]  # batch 1
+        #    ...
+        #]
+        self.randomBatchList = [[[None] for _1 in range (self.amountOfDataVectors)] for _ in range(self.numberOfBatches)] #Allocate memory
+        self.randomIndexList = []
+        self.currentBatch = 0
+
 
     def evaluate_loss(self, position):
         """
@@ -24,7 +48,17 @@ class LossObj:
         return np.array([])
 
     def fillRandomBatchList(self):
-        pass
-        #self.data = random.shuffle(self.data)
 
-        #self.
+        self.randomIndexList = np.random.permutation(self.xDataLength)
+
+        #Shuffles the data in each data vector but keeps related values in appropriate places
+        for i in range(self.amountOfDataVectors):
+            self.shuffledData[i] = self.data[i][self.randomIndexList]
+
+
+        #Fills the randomBatchList with batches from the shuffled data
+        for i in range(0, self.xDataLength, self.batchAmount):
+            for j in range(self.amountOfDataVectors):
+                self.randomBatchList[i][j] = self.shuffledData[j][i: i + self.batchAmount]
+
+        return
