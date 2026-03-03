098195427cc1ae0dffaef7aa4adc4bb50567a2a7
Hannes <257142595+hgb-dev@users.noreply.github.com>
2026-03-03 15:42:52 +0100

    Rosenbrock 2D

--- Rosenbrock.py
+++ Rosenbrock.py
@@ -2,11 +2,18 @@ import numpy as np
 from optimizers.loss.loss import LossObj
 
 class Rosenbrock(LossObj):
-    def __init__(self):
-        pass
+
+    #f(x,y)={(a-x)^2}+b{(y-{x^2})^2}}
+    def __init__(self, a=1, b=100):
+        self.a = a
+        self.b = b
+
 
     def evaluate_loss(self, x):
-        pass
+        #x = pos
+
+        return ((self.a - x[0]) ** 2) + self.b * ((x[1] - (x[1] - (x[0] ** 2))) ** 2)
 
     def evaluate_gradient(self, x, batch=None):
-        pass
+        #grad((a - x)^2 + b (y - x^2)^2) = (-2 a + 2 x + 4 b x^3 - 4 b x y, 2 b (- x^2 + y))
+        return np.array([-2 * self.a + 2 * x[0] + 4 * self.b * (x[0] ** 3) - 4 * self.b * x[0] * x[1], 2 * self.b * (- (x[0] ** 2) + x[1]) ])
