51a2c4bc96892116d0f415794f9e26fb21aa45fd
Hannes <257142595+hgb-dev@users.noreply.github.com>
2026-03-04 14:46:42 +0100

    Alternative Logistical regression evaluate loss function

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -8,6 +8,12 @@ class LogisticRegression(LossObj):
     def evaluate_loss(self, position):
         return np.mean(np.log(1 + np.exp(-self.data[:, 1] * (self.data[:, 0] @ position))))
 
+    def evaluate_loss2(self, weights):
+
+        x = np.numarray(self.data[0])
+        #Might be more stable if another log programming function is used.
+        return (1/self.xDataLength) * np.sum(np.log(1 + np.exp(-self.data[1] * (x @ weights))))
+
     def evaluate_gradient(self, position, batch=None):
         batch = np.array(self.randomBatchList[self.currentBatch])
         return -(batch[:, 0].T @ (batch[:, 1] / (1 + np.exp(batch[:, 1] * (batch[:, 0] @ position))))) / len(batch)
