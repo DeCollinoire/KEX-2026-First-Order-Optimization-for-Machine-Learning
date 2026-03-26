b59f56051cea1caf83cf73bbb3543e87980948fb
Melvin Blackhammar <85481313+DeCollinoire@users.noreply.github.com>
2026-03-26 13:37:32 +0100

    Fixed mistake in imports

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import 
 
 def plotPath(qdf, history, optimizer_name, center = [0,0], scale: float = 1):
     # Create a grid of points
