402dc888d9b07c3f95d50bc44ab2110e2ac97daf
decollinoire <melvin.blackh@gmail.com>
2026-04-30 13:21:59 +0200

    Enhanced createVariants with problem-specific configurations

--- sensitivity.py
+++ sensitivity.py
@@ -17,7 +17,7 @@ All optimizer variants are created first, as opposed to modifying them one-by-on
 
 class LogisticRegression_ExternalBatching():
     """ 
-    This version of logistic regression externalizes batching from the lossObj, which allows us to use the same lossObj for all optimizers and batch sizes in the sensitivity test.
+    This version of logistic regression externalizes batching from the lossObj.
     """
     def __init__(self, X, y):
         self.X = X
@@ -37,72 +37,109 @@ class LogisticRegression_ExternalBatching():
         X, y = self.getCurrentBatch()
         return -(X.T @ (y * expit(-(y * (X @ weights))))) # expit is a sigmoid function
 
-def createVariants(lossObj, initPos, rcv1_on = True):
-    variants = {"SGD": [], "Nesterov": [], "Momentum": [], "Adam": []}
+import itertools
+
+import itertools
+
+def createVariants(lossObj, initPos, problemName="australian"):
+    # Master configuration for each problem
+    configs = {
+        "australian": {
+            "SGD": {"lr": [0.01, 0.02, 0.03, 0.04, 0.05, 0.06]},
+            "Nesterov": {
+                "lr": [0.02, 0.03, 0.04, 0.05, 0.06],
+                "df": [0.5, 0.7, 0.9, 0.99]
+            },
+            "Adam": {
+                "lr": [0.1, 0.5, 1.0],
+                "fm": [0.9, 0.99],
+                "fr": [0.9, 0.99]
+            }
+        },
+        "rcv1": {
+            "SGD": {"lr": [0.01, 0.025, 0.05, 0.075, 0.1]},
+            "Nesterov": {
+                "lr": [0.01, 0.025, 0.05, 0.075, 0.1],
+                "df": [0.5, 0.8, 0.95, 0.999]
+            },
+            "Adam": {
+                "lr": [0.01, 0.1, 0.5],
+                "fm": [0.4, 0.7, 0.9],
+                "fr": [0.4, 0.7, 0.9]
+            }
+        },
+        "Rosenbrock": {
+            "SGD": {
+                "lr": [0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.0009, 0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008] 
+            },
+            "Nesterov": {
+                "lr": [0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.0009, 0.001],
+                "df": [0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]
+            },
+            "Adam": {
+                "lr": [0.004, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01],
+                "fm": [0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999],
+                "fr": [0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]
+            }
+        }
+    }
+
+    # Extract the specific config for the current problem
+    problemConfig = configs.get(problemName, configs["australian"]) # default to australian
+    variants = {opt: [] for opt in ["SGD", "Nesterov", "Momentum", "Adam"]}
 
     # SGD
-    lrList = [0.01, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 0.05, 0.055, 0.06]
-    for lr in lrList:
+    for lr in problemConfig["SGD"]["lr"]:
         variants["SGD"].append(sgd.SGD(lossObj, initPos, lr=lr))
 
-    # Nesterov
-    if rcv1_on:
-        lrList = [0.01, 0.025, 0.05, 0.075, 0.1]
-        decayFactorList = [0.5, 0.6, 0.7, 0.75, 0.8, 0.85, 0.9, 0.99]
-    else:
-        lrList = [0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 0.05, 0.055, 0.06]
-        decayFactorList = [0.5, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.999]
-    for lr in lrList:
-        for decayFactor in decayFactorList:
-            variants["Nesterov"].append(nesterov.Nesterov(lossObj, initPos, lr=lr, decayFactor=decayFactor))
-    
-    # Momentum
-    # Use same settings as Nesterov
-    for lr in lrList:
-        for decayFactor in decayFactorList:
-            variants["Momentum"].append(momentum.Momentum(lossObj, initPos, learningRate=lr, decayFactor=decayFactor))
+    # Nesterov & Momentum (sharing Nesterov config)
+    nesterovConfig = problemConfig["Nesterov"]
+    for lr, df in itertools.product(nesterovConfig["lr"], nesterovConfig["df"]):
+        variants["Nesterov"].append(nesterov.Nesterov(lossObj, initPos, lr=lr, decayFactor=df))
+        variants["Momentum"].append(momentum.Momentum(lossObj, initPos, learningRate=lr, decayFactor=df))
 
     # Adam
-    if rcv1_on:
-        lrList = [0.1, 0.5, 0.85, 0.1, 0.5, 1]
-        forgettingFactorMList = [0.7, 0.8, 0.9, 0.99]
-        forgettingFactorRList = [0.7, 0.8, 0.9, 0.99]
-    else:
-        lrList = [0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75]
-        forgettingFactorMList = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
-        forgettingFactorRList = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
-    for lr in lrList:
-        for forgettingFactorM in forgettingFactorMList:
-            for forgettingFactorR in forgettingFactorRList:
-                variants["Adam"].append(adam.Adam(lossObj, initPos, learningRate=lr, forgettingFactorM=forgettingFactorM, forgettingFactorR=forgettingFactorR))
+    adamConfig = problemConfig["Adam"]
+    for lr, fm, fr in itertools.product(adamConfig["lr"], adamConfig["fm"], adamConfig["fr"]):
+        variants["Adam"].append(adam.Adam(lossObj, initPos, learningRate=lr, forgettingFactorM=fm, forgettingFactorR=fr))
 
     return variants
+
+
+datasetMap = {
+    "Rosenbrock": "N/A",
+    "australian": "datasets/australian",
+    "rcv1": "datasets/rcv1_train.binary"
+}
 
 def main():
     # Config
     randomSeed = 10
-    datasetFilepath = "datasets/australian_scale" # This is also used for plot titles
+    problemName = "Rosenbrock"
+    datasetFilepath = datasetMap[problemName] # This is also used for plot titles
+    batchSize = 100000 # None means fullbatch, only used by LogReg
     initialPosInterval = 0
-    batchSize = 100000
-    l2NormalizationOn = (datasetFilepath == "datasets/australian") or (datasetFilepath == "datasets/australian_scale")
+    l2NormalizationOn = (problemName == "australian") or (datasetFilepath == "australian_scale")
+    dim = 10 # For Rosenbrock only
+    nrEpochs = 20
+    printProgress=True
 
     # Setup
     print("Setting up...")
     np.random.seed(randomSeed)
-    X, y = loadDataAsNumpyArray(datasetFilepath, toDense=False, l2NormalizationOn=l2NormalizationOn)
-    nrSamples, nrFeatures = X.shape # type: ignore
-
-    lossObj = LogisticRegression_ExternalBatching(X, y) # Use the external batching version of logistic regression for this test. It works fine to use the internal batching of the lossObj also.
-    initPos = np.random.uniform(-initialPosInterval, initialPosInterval, nrFeatures) # Initialize position based on the number of features in the dataset
+    lossObj, initPos = setupProblem(problemName=problemName, dim=dim, datasetFilepath=datasetFilepath,
+                                    initialPosInterval=initialPosInterval, randomSeed=randomSeed,
+                                    batchSize=batchSize, toDense=False, l2NormalizationOn=l2NormalizationOn)  # QDF, Rosenbrock; datasetFilepath is only needed for LogReg
 
     # Create variants
-    variants = createVariants(lossObj, initPos, rcv1_on = not l2NormalizationOn) # Creates all variants of sgd, nesterov, momentum and adam in a dictionary
+    variants = createVariants(lossObj, initPos, problemName=problemName) # Creates all variants of sgd, nesterov, momentum and adam in a dictionary
     joinedOptList = [opt for optList in variants.values() for opt in optList] # Join all optimizers into one list for training
     print(f"Setup finished!")
 
     # Train
     print(f"\n--- Training ---")
-    train_external_batching(joinedOptList, lossObj, X, y, nrEpochs=20, batchSize=batchSize, printProgress=True)
+    # train_external_batching(joinedOptList, lossObj, X, y, nrEpochs=nrEpochs, batchSize=batchSize, printProgress=printProgress)
+    train(joinedOptList, lossObj, nrEpochs=nrEpochs, printProgress=printProgress)
     print(f"Training finished.")
 
     # Plot
@@ -114,7 +151,7 @@ def main():
             # Pass the hyperparameter string into the label or a custom attribute
             label_str = f"{opt.__class__.__name__}: {opt.getHyperparamStr()}"
             line = plotHistoryGraph(opt.lossHistory, 
-                                    title=f"{optClassName} Sensitivity, dataset = {datasetFilepath}", 
+                                    title=f"{optClassName} Sensitivity, {problemName}, dataset = {datasetFilepath}, batchSize = {"fullbatch" if batchSize is None else batchSize}", 
                                     label=label_str, 
                                     ylabel="Loss",
                                     legendOn=False # use external legend
